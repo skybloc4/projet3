071b76a05c7a6501e29d842bc76c433e14458a69
skybloc4 <lsaintomer@gaming.tech>
2022-11-08 12:12:08 +0100

    yo

---
+++ test.py
@@ -0,0 +1,56 @@
+DEBUT
+
+# print("hello world")
+r = 12000 
+s = 1250
+e = 10
+rh = 230 
+assertUn = (( (365 * 3) / (24 - (16 - 8))) * (rh) > r ) == (e * s < r)# false
+assertPrunUn = (( (365 * 3) / (24 - (16 - 8))) * (rh) > r )# true
+assertPrunDeux = (e * s < r)# false
+#assertionUn = assertDeux == assertTrois # false 
+print(assertDeux == assertTrois)
+
+
+
+assertDeux = (( 365 * 3) / (4 - (12 - 8)) * (rh) > r ) == (e * s < r)# true
+assertPrdeuxUn = (( 365 * 3) / (4 - (12 - 8)) * (rh) > r )# false
+assertPrdeuxDeux = (e * s < r)# false
+
+x = 3
+
+def retournerSixPlusTrois():
+    return 6 + 3
+def retournerSixPlusX(x){
+    return 6 + x 
+}
+
+print("Qui vole un " + retournerSixPlusTrois() + " vole un boeuf ")
+print(retournerSixPlusX(3))
+
+def add(x,y)
+    return x,y
+
+def sub(x,y)
+    return x,y 
+
+def multiply(x,y)
+    return x,y
+
+def divide(x,y)
+    return x,y
+
+def modulo(x,y)
+    return x,y
+
+def calculsalaireparseconde(
+salairehoraire,
+heureparjoursouvrable
+nbjoursouvrable)
+
+
+
+def calculsalairenet(
+    salairebrut, coeff )
+
+FIN
